ca797b8b079c896b850422e4a02a30931111ecb5
whybin <31753349+whybin@users.noreply.github.com>
2017-10-10 02:29:53 -0400

    Fix data conversion

--- gulp/convert-data.js
+++ gulp/convert-data.js
@@ -4,15 +4,12 @@ const Through    = require('through2');
 const convertData = function (file, encoding, callback) {
     let contents  = file.contents.toString();
     let json      = JSON.parse(contents)['_default'];
-    let data      = [];
-    for (let i in json) {
-        data.push(json[i]);
-    }
+    let stem      = file.relative.split('.')[0];
+    contents      = 'this.DATA = this.DATA || {}; this.DATA["' + stem + '"] = '
+        + JSON.stringify(Object.values(json)) + ';';
 
-    contents      = JSON.stringify(data);
     file.contents = Buffer.from(contents);
     file.path     = file.path.replace('.json', '.js');
-
 
     this.push(file);
     callback();
